2a8426d737c9f4a0116a67a2ae2545f78cda0b5a
Alexander J Franco <lxfranco1@gmail.com>
2019-10-04 11:00:17 -0400

    Updated Jet class to completed version
    The Jet class is now completed with comments on non-redundant functions

--- Jet.cpp
+++ Jet.cpp
@@ -4,12 +4,14 @@
 
 #include "Jet.h"
 #include <math.h>
+#include <string.h>
 Jet::Jet() {
     setNumberOfEngines(1);
     setBrand("Custom");
     setModel("VTx");
 }
 
+//Jet has added parameter number of engines
 Jet::Jet(string brand, string model, string fuelType, int engines) {
     setBrand(brand);
     setModel(model);
@@ -30,9 +32,9 @@ void Jet::setNumberOfEngines(int engines) {
 
 double Jet::mileageEstimate(double time) {
   double ran =rand() % 60 + 40;
-  ran =floor(ran);
+  ran =floor(ran);                  //floor random number
     double mileage =time *ran  ;
-    if(getNumberOfEngines()>2){
+    if(getNumberOfEngines()>2&&getFuelType().compare("Rocket")==0){     //5 percent increase for every engine
         mileage+= ((mileage*.055)*getNumberOfEngines());
 
     }
@@ -40,6 +42,7 @@ double Jet::mileageEstimate(double time) {
     return mileage;
 }
 
+//to string edited to cats int as a string for cuntionality
 string Jet::toString() {
   return "-> Jet\n" + PoweredVehicle::toString() + "\n\tEngine Number: " +
          std::to_string(getNumberOfEngines());
